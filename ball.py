e3d465f095bd9229942cfbdb2ed37cac232701bb
Will Taylor <w1ll100@hotmail.co.uk>
2019-01-01 15:33:29 +0000

    Semi-implicit Euler gravity

--- ball.py
+++ ball.py
@@ -1,22 +1,39 @@
 import random
 import math
+from particle import Particle
 
 #Simulating mass as proportional to area
 def r_to_m(r):
 	return math.pi * r**2
 
-class Ball:
+class Ball(Particle):
 	def __init__(self, x = 0, y = 0, r = 1):
 		self.x = x
 		self.y = y
 		self.r = r
-		self.m =r_to_m(r)
-		self.x_vel = random.uniform(-0.2, 0.2)
-		self.y_vel = random.uniform(-0.2, 0.2)
+		self.m = r_to_m(r)
+		self.x_vel = random.uniform(-0.1, 0.1)
+		self.y_vel = random.uniform(-0.1, 0.1)
 
-	def update(self, x_force, y_force):
+	def update_pos(self):
 		self.x += self.x_vel
 		self.y += self.y_vel
 
-		self.x_vel += x_force / self.m
-		self.y_vel += y_force / self.m
+	def update_vel(self, x_f, y_f):
+		self.x_vel += x_f / self.m
+		self.y_vel += y_f / self.m
+
+	def update(self, x_force, y_force):
+		self.update_vel(x_force, y_force)
+		self.update_pos()
+
+	def vec_to(self, ball):
+		return abs(self.x - ball.x), abs(self.y - ball.y)
+
+	def distance_to_squared(self, ball):
+		x_d, y_d = self.vec_to(ball)
+		return x_d**2 + y_d**2
+
+	def distance_to(self, ball):
+		x_d, y_d = self.vec_to(ball)
+		return math.sqrt(x_d**2 + y_d**2)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from pygame.locals import *
 import random
 import new_balls
+import os
+import math
 
 def keep_running(events):
 	for event in events:
@@ -13,14 +15,42 @@ def keep_running(events):
 			return False
 	return True
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-MAX_BALL_RADIUS = 25
-running = True
+def initial_balls():
+	MAX_BALL_RADIUS = 25
+	return [Ball(random.randint(MAX_BALL_RADIUS, SCREEN_WIDTH - MAX_BALL_RADIUS), random.randint(MAX_BALL_RADIUS, SCREEN_HEIGHT - MAX_BALL_RADIUS), random.randint(2, MAX_BALL_RADIUS)) for i in range(10)]
+
+def calc_grav_force(ball, grav_ball):
+	f = 0.008 * (ball.m * grav_ball.m) / ball.distance_to_squared(grav_ball)
+	x_d = grav_ball.x - ball.x
+	y_d = grav_ball.y - ball.y
+	theta = math.atan(abs(y_d) / abs(x_d))
+	x_f = (f * (math.cos(theta))) * (1 if x_d >= 0 else -1)
+	y_f = (f * (math.sin(theta))) * (1 if y_d >= 0 else -1)
+	return x_f, y_f
+
+def update_ball(ball, grav_balls):
+	#Pull to centre, further = more force
+	#vec_to_centre = (((SCREEN_WIDTH / 2) - ball.x) / 5000, ((SCREEN_HEIGHT / 2) - ball.y) / 5000)
+
+	#Total gravitational pull to grav_balls
+	total_x_f = total_y_f = 0
+	for grav_ball in grav_balls:
+		x_f, y_f = calc_grav_force(ball, grav_ball)
+		total_x_f += x_f
+		total_y_f += y_f
+
+	ball.update(total_x_f, total_y_f)
+
+#Set window position on screen
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50, 50)
 pygame.init()
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 900
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-balls = [Ball(random.randint(MAX_BALL_RADIUS, SCREEN_WIDTH - MAX_BALL_RADIUS), random.randint(MAX_BALL_RADIUS, SCREEN_HEIGHT - MAX_BALL_RADIUS), random.randint(2, MAX_BALL_RADIUS)) for i in range(10)]
+running = True
+balls = initial_balls()
+grav_balls = [Ball(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, 10), Ball(2 * SCREEN_WIDTH / 3, 2 * SCREEN_HEIGHT / 3, 10)]
 
 new_ball_handler = new_balls.BallSpawner()
 
@@ -31,12 +61,14 @@ while running:
 	new_ball_handler.spawn_new_balls(pygame.mouse.get_pressed()[0], balls)
 
 	for ball in balls:
-		vec_to_centre = (((SCREEN_WIDTH / 2) - ball.x) / 5000, ((SCREEN_HEIGHT / 2) - ball.y) / 5000)
-		ball.update(vec_to_centre[0], vec_to_centre[1])
+		update_ball(ball, grav_balls)
 
 	screen.fill((0,0,0))
 
 	for ball in balls:
 		pygame.draw.circle(screen, (255, 255, 255), (round(ball.x), round(ball.y)), round(ball.r))
+
+	for grav_ball in grav_balls:
+		pygame.draw.circle(screen, (255, 0, 0), (round(grav_ball.x), round(grav_ball.y)), round(grav_ball.r))
 
 	pygame.display.flip()
