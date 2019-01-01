fc06000569a5fe689c6e598fd067d20f56993a95
Will Taylor <w1ll100@hotmail.co.uk>
2019-01-01 22:00:02 +0000

    Moved initial_balls to new_balls

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ from ball import Ball
 from game_state import GameState
 from physics import update_ball
 
-def initial_balls():
-	MAX_BALL_RADIUS = 25
-	return [Ball(random.randint(MAX_BALL_RADIUS, SCREEN_WIDTH - MAX_BALL_RADIUS), random.randint(MAX_BALL_RADIUS, SCREEN_HEIGHT - MAX_BALL_RADIUS), random.randint(2, MAX_BALL_RADIUS)) for i in range(10)]
-
 #Set window position on screen
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50, 50)
 pygame.init()
@@ -20,7 +16,7 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 900
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-balls = initial_balls()
+balls = new_balls.initial_balls(SCREEN_WIDTH, SCREEN_HEIGHT)
 grav_balls = [Ball(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, 10), Ball(2 * SCREEN_WIDTH / 3, 2 * SCREEN_HEIGHT / 3, 10)]
 
 game_state = GameState()
--- new_balls.py
+++ new_balls.py
@@ -2,9 +2,12 @@ import pygame
 import random
 from ball import Ball
 
+def initial_balls(SCREEN_WIDTH, SCREEN_HEIGHT):
+	MAX_RADIUS = 25
+	return [Ball(random.randint(MAX_RADIUS, SCREEN_WIDTH - MAX_RADIUS), random.randint(MAX_RADIUS, SCREEN_HEIGHT - MAX_RADIUS), random.randint(2, MAX_RADIUS)) for i in range(10)]
+
 MAX_BALLS = 100
 NEW_BALL_RATE_LIMIT = 50
-
 class BallSpawner:
 	def __init__(self):
 		self.time_since_last_ball = NEW_BALL_RATE_LIMIT
