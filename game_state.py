b4bec231e678fd806e20b5a38aa1871e80f4f6d1
Will Taylor <w1ll100@hotmail.co.uk>
2019-01-02 21:28:21 +0000

    Moved modes to game_state

--- game_state.py
+++ game_state.py
@@ -4,34 +4,47 @@ from pygame.locals import *
 from spawner import NewObject
 from screen_painter import Colour
 
-class GameState:
+class Modes:
 	def __init__(self):
+		self.ball_colour = Colour.INITIAL
+		self.click_spawn_behaviour = NewObject.INITIAL
+
+	def set(self, value_to_set):
+		if isinstance(value_to_set, Colour):
+			self.ball_colour = value_to_set
+		elif isinstance(value_to_set, NewObject):
+			self.click_spawn_behaviour = value_to_set
+		else:
+			raise Exception("Invalid mode_to_toggle_to " + mode_to_toggle_to + " passed to modes.set_from_handled_inputs()")
+
+class GameState:
+	mode_toggle_keys = { K_v : NewObject.BALL,
+						 K_f : NewObject.GRAV_BALL,
+						 K_r : Colour.RED,
+						 K_g : Colour.GREEN,
+						 K_b : Colour.BLUE,
+						 K_t : Colour.GREY }
+
+	def __init__(self):
+		self.modes = Modes()
 		self.running = True
 		self.paused = False
 		self.click_spawns = NewObject.INITIAL
 
 	def handle_inputs(self, events):
-		modes_to_set = []
 		for event in events:
 			if event.type == KEYDOWN:
 				if event.key == K_q:
 					self.running = False
 				elif event.key == K_SPACE:
 					self.paused = False if self.paused else True
-
-				if event.key == K_v:
-					modes_to_set.append(NewObject.BALL)
-				elif event.key == K_f:
-					modes_to_set.append(NewObject.GRAV_BALL)
-				elif event.key == K_r:
-					modes_to_set.append(Colour.RED)
-				elif event.key == K_g:
-					modes_to_set.append(Colour.GREEN)
-				elif event.key == K_b:
-					modes_to_set.append(Colour.BLUE)
-				elif event.key == K_t:
-					modes_to_set.append(Colour.GREY)
-
+				elif event.key in GameState.mode_toggle_keys.keys():
+					self.modes.set(GameState.mode_toggle_keys[event.key])
 			elif event.type == QUIT:
 				self.running = False
-		return modes_to_set
+
+	def get_ball_colour(self):
+		return self.modes.ball_colour
+
+	def get_click_spawn_behaviour(self):
+		return self.modes.click_spawn_behaviour
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import screen_painter
 from ball import Ball
 from game_state import GameState
 from physics import update_ball
-from modes import Modes
 
 #Set window position on screen
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50, 50)
@@ -21,22 +20,21 @@ balls = initial_balls(SCREEN_WIDTH, SCREEN_HEIGHT)
 grav_balls = [Ball(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, 10), Ball(2 * SCREEN_WIDTH / 3, 2 * SCREEN_HEIGHT / 3, 10)]
 
 game_state = GameState()
-modes = Modes()
 new_ball_handler = Spawner()
 painter = screen_painter.ScreenPainter()
 painter.ball_painting_algorithm = screen_painter.speed
 
 while game_state.running:
-	modes.set_from_handled_inputs(game_state.handle_inputs(pygame.event.get()))
+	game_state.handle_inputs(pygame.event.get())
 
 	if not game_state.paused:
-		new_ball_handler.update(game_state.click_spawns)
+		new_ball_handler.update(game_state.get_click_spawn_behaviour())
 		if pygame.mouse.get_pressed()[0]:
 			new_ball_handler.spawn(balls, grav_balls)
 
 		for ball in balls:
 			update_ball(ball, grav_balls)
 
-		painter.paint(screen, balls, grav_balls, modes.ball_colour)
+		painter.paint(screen, balls, grav_balls, game_state.get_ball_colour())
 
 		pygame.display.flip()
